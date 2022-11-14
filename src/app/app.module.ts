2403d327f8f6c3b6135f6c9a26d21275b5d5a671
Jonatan Tiago <jonatan2206@gmail.com>
2022-11-14 17:25:25 -0300

    Search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [
@@ -16,7 +17,7 @@ import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.com
 		HomeComponent,
 		ThemeToggleComponent
 	],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [BrowserModule, AppRoutingModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+	searchQuery = '';
+
 	constructor() {}
 
 	ngOnInit(): void {}
