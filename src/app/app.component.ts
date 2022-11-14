5a99dde87df4d6c89f0a6c94b1a6f5f90a084f76
Jonatan Tiago <jonatan2206@gmail.com>
2022-11-14 14:29:29 -0300

    Applying light or dark theme depending on user device

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'tmdb-app';
+
+	ngOnInit(): void {
+		let themePreference = localStorage.getItem('darkMode');
+		if (themePreference !== null) {
+			this.changeTheme(themePreference === '1');
+		} else {
+			this.changeTheme(
+				window.matchMedia('(prefers-color-scheme: dark').matches
+			);
+		}
+	}
+
+	private changeTheme(isDarkMode: boolean): void {
+		if (isDarkMode) {
+			document.body.classList.remove('lightmode');
+			document.body.classList.add('darkmode');
+			document.documentElement.style.setProperty('color-scheme', 'dark');
+		} else {
+			document.body.classList.remove('darkmode');
+			document.body.classList.add('lightmode');
+			document.documentElement.style.setProperty('color-scheme', 'light');
+		}
+		localStorage.setItem('darkMode', '1');
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, HeaderComponent, CardComponent, HomeComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent]
