103d4a4789ea6f62004e295d9630b48a51071553
Jonatan Tiago <jonatan2206@gmail.com>
2022-11-21 16:06:55 -0300

    Setting up ESLint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-	{
-		path: '',
-		loadChildren: () =>
-			import('./pages/home/home.module').then(m => m.HomeModule)
-	}
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then(m => m.HomeModule)
+  }
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
