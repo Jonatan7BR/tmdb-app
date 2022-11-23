f06156eb72a420a6c4f4b26bcb7c02281ff5ae56
Jonatan Tiago <jonatan2206@gmail.com>
2022-11-23 10:18:33 -0300

    Trending movies at home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { Movie } from 'src/app/models/movie.model';
 import { MovieService } from 'src/app/services/movie.service';
@@ -11,22 +11,19 @@ import { POSTER_BASE_URL } from 'src/app/utils/tmdb.utils';
 })
 export class HomeComponent implements OnInit {
 
-  @ViewChild('searchResults') searchResults!: ElementRef<HTMLDivElement>;
-
-  searchQuery = '';
   currentPage = 1;
   totalPages = 0;
-  noResults = false;
-  searched = '';
   movies: Movie[] = [];
 
   constructor (private movieService: MovieService) {}
 
-  ngOnInit (): void {}
+  ngOnInit (): void {
+    this.fetchMovies();
+  }
 
   fetchMovies (): void {
     this.movieService
-      .getMovies(this.searchQuery, this.currentPage)
+      .getTrending()
       .subscribe((response): void => {
         this.totalPages = response.total_pages;
         this.movies = response.results.map((result): Movie => ({
@@ -35,15 +32,7 @@ export class HomeComponent implements OnInit {
           releaseDate: moment(result.release_date).toDate(),
           overview: result.overview
         }));
-        this.noResults = this.movies.length === 0;
-        this.searched = this.searchQuery;
       });
-  }
-
-  changePage (page: number): void {
-    this.currentPage = page;
-    this.searchResults.nativeElement.scrollIntoView({ behavior: 'smooth' });
-    this.fetchMovies();
   }
 
 }
